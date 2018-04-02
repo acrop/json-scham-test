e46c2e5a8a6ea42f6ad616b47441da701b291586
Yonggang Luo <luoyonggang@gmail.com>
2018-04-02 22:17:51 +0800

    Add json-schema support.

--- src/app/ReactComponent.ts
+++ src/app/ReactComponent.ts
@@ -4,7 +4,19 @@ import {
 } from '@angular/core';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {HelloReact, HelloProps} from './HelloReact';
+import { HelloReact, HelloProps } from './HelloReact';
+import Form, { FormProps } from 'react-jsonschema-form';
+import { JSONSchema6 } from 'json-schema';
+
+const schema: JSONSchema6 = {
+    title: 'Todo',
+    type: 'object',
+    required: ['title'],
+    properties: {
+        title: { type: 'string', title: 'Title', default: 'A new task' },
+        done: { type: 'boolean', title: 'Done?', default: false }
+    }
+};
 
 @Component({
     selector: 'app-react-component',
@@ -24,8 +36,11 @@ export class ReactComponent implements OnDestroy, OnChanges {
     render() {
         console.log('ReactComponent render');
         console.log(JSON.stringify(this.props));
-        ReactDOM.render(React.createElement(HelloReact, this.props),
-            this._element);
+        const formProps: FormProps = {
+            schema: schema
+        };
+
+        ReactDOM.render(React.createElement(Form, formProps), this._element);
     }
 
     ngOnChange(changes) {
